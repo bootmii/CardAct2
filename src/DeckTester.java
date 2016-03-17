f8ba72fa218061c6f432cb9fd75ac651c8b1cbe4
116060088 <alt.people.davidcalman@gmail.com>
2016-03-17 08:57:35 -0700

    test deck?

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -8,6 +8,34 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks1 = {"i","ro","ha"};
+		String[] suits1= {"ki","hi","tsuchi","kane","mizu"};
+		int[] values1 = {1,2,5};
+		Deck testDeck1 = new Deck(ranks1, suits1, values1);
+		testDeck1.size();
+		testDeck1.isEmpty();
+		testDeck1.deal();
+		testDeck1.toString();
+		testDeck1.size();
+		testDeck1.deal();
+		testDeck1.size();
+		String[] ranks2 = {"Nine","Ten","Jack","Queen","King","Ace"};
+		String[] suits2 = {"Spades", "Hearts", "Diamonds", "Clubs"};
+		int[] values2 = {9,10,11,12,13,14};
+		Deck euchre = new Deck(ranks2, suits2, values2);
+		euchre.size();
+		euchre.toString();
+		euchre.deal();
+		euchre.isEmpty();
+		euchre.deal();
+		euchre.size();
+		String[] ranks3 = {"Two", "Three", "Four", "Five", "Six","Seven",
+				"Eight","Nine","Ten","Jack","Queen","King","Ace"};
+		int[] values3 = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+		Deck poker = new Deck(ranks3,suits2,values3);
+		poker.toString();
+		poker.isEmpty();
+		poker.deal();
+		poker.size();
 	}
 }
