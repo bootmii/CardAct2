5ae3be9e40f2ea9491cd45fef1449d369d66679c
116060088 <alt.people.davidcalman@gmail.com>
2016-04-07 08:50:19 -0700

    rename

--- src/CardGUIRunner.java
+++ src/CardGUIRunner.java
@@ -2,7 +2,7 @@
  * This is a class that plays the GUI version of the Elevens game.
  * See accompanying documents for a description of how Elevens is played.
  */
-public class ElevensGUIRunner {
+public class CardGUIRunner {
 
 	/**
 	 * Plays the GUI version of Elevens.
