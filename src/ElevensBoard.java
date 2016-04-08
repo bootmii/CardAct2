6d3408c0e9ccbd771310584dd29f60f7eb062c39
116060088 <alt.people.davidcalman@gmail.com>
2016-04-08 09:06:49 -0700

    debug

--- src/ElevensBoard.java
+++ src/ElevensBoard.java
@@ -30,6 +30,8 @@ public class ElevensBoard extends Board {
 	 */
 	private static final int[] POINT_VALUES =
 		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
+	
+	private static final boolean I_AM_DEBUGGING = true;
 
 	/**
 	 * Creates a new <code>ElevensBoard</code> instance.
